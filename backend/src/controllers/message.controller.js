086a688644b305508f7ac2cf1d7b95c9bc6f3630
Huong <huongnguyen.nth1709@gmail.com>
2025-03-19 15:49:10 +0700

    implement the send message route and controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
 import Message from '../models/message.model.js';
+import cloudinary from '../lib/cloudinary.js';
 
 // get Users except the loggedIn User
 export const getUsers = async (req, res) => {
@@ -32,6 +33,37 @@ export const getMessages = async (req, res) => {
     res.status(200).json(messages);
   } catch (error) {
     console.error('Error in getMessages controller: ', error.message);
+    res.status(500).json({ message: `Internal Error: ${error.message}` });
+  }
+};
+
+// send Message
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params.id;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    // todo: realtime functionality goes here => socket.io
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.error('Error in sendMessage controller: ', error.message);
     res.status(500).json({ message: `Internal Error: ${error.message}` });
   }
 };
